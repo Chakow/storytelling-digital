c1474008b4ded9e35ff10285bfd938ddda2e4703
chako <charlot.kolly@studentfr.ch>
2025-11-17 14:34:01 +0100

    ajout d'inertie au scroll et ajustement des durées des animations

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,14 @@ document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     document.querySelector(".loading-page").remove();
 
+    const smoother = ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1.5, // inertie (= plus c’est haut, plus c’est doux)
+      smoothTouch: 0.1, // éviter l'effet trop glissant sur mobile
+      effects: true,
+    });
+
     var timelineFirstPart = gsap.timeline({
       scrollTrigger: {
         trigger: ".first-part",
@@ -25,8 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //accueil crevasse
     timelineFirstPart.to(".crevasse", {
       scale: 4.5,
-      ease: "power2.out",
-      duration: 5,
+      duration: 20,
     });
     timelineFirstPart.to(".crevasse", {
       display: "none",
@@ -39,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     timelineFirstPart.to(".crique", {
       y: "33%",
-      duration: 1,
+      duration: 2,
     });
     //dsiparition crique et titre
     timelineFirstPart.to(".crique", {
@@ -52,55 +59,59 @@ document.addEventListener("DOMContentLoaded", function () {
       {
         y: "-250%",
         display: "none",
-        duration: 1,
+        duration: 2,
       },
       "<"
     );
     //premier texte narration
     timelineFirstPart.to(".first-narration-text", {
       opacity: "1",
-      duration: 1,
-    });
-    timelineFirstPart.to(".first-narration-text", {
-      opacity: "1",
       duration: 2,
     });
     timelineFirstPart.to(".first-narration-text", {
-      opacity: "0",
+      opacity: "1",
       duration: 1,
+    });
+    timelineFirstPart.to(".first-narration-text", {
+      opacity: "0",
+      duration: 2,
     });
     //apparition première famille
     timelineFirstPart.to(".first-family", {
       y: "0%",
-      duration: 1,
+      duration: 2,
     });
     //disparition première famille
     timelineFirstPart.to(".second-plan", {
       x: "70%",
       y: "8%",
-      duration: 3,
+      duration: 15,
     });
+    timelineFirstPart.to(
+      ".speech-bubbles-first-family",
+      {
+        opacity: "1",
+        duration: 15,
+      },
+      "<"
+    );
     timelineFirstPart.to(".speech-bubbles-first-family", {
       opacity: "1",
-      duration: 1,
-    });
-    timelineFirstPart.to(".speech-bubbles-first-family", {
-      opacity: "1",
-      duration: 1,
+      duration: 2,
     });
     timelineFirstPart.to(".speech-bubbles-first-family", {
       opacity: "0",
-      duration: 0.5,
+      duration: 2,
     });
     timelineFirstPart.to(".second-plan", {
       y: "155%",
-      duration: 1,
+      duration: 5,
     });
     timelineFirstPart.to(
       ".foreground",
       {
         y: "75%",
-        duration: 1,
+        duration: 5,
       },
       "<"
     );
@@ -108,7 +119,7 @@ document.addEventListener("DOMContentLoaded", function () {
       ".background",
       {
         y: "75%",
-        duration: 1,
+        duration: 5,
       },
       "<"
     );
@@ -117,7 +128,7 @@ document.addEventListener("DOMContentLoaded", function () {
       ".first-part",
       {
         backgroundColor: "black",
-        duration: 1,
+        duration: 2,
       },
       "<"
     );
@@ -130,31 +141,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
     timelineFirstPart.to(".second-family", {
       y: "0%",
-      duration: 1,
+      duration: 5,
     });
     timelineFirstPart.to(".second-family-second-plan", {
       x: "-15%",
       y: "7%",
-      duration: 3,
+      duration: 15,
     });
     timelineFirstPart.to(
       ".second-family-dad-looking",
       {
         x: "0%",
-        duration: 3,
+        duration: 15,
       },
       "<"
     );
     //disparition deuxième famille
     timelineFirstPart.to(".second-family-dad-looking", {
       y: "100%",
-      duration: 1,
+      duration: 5,
     });
     timelineFirstPart.to(
       ".second-family-second-plan",
       {
         y: "100%",
-        duration: 1,
+        duration: 5,
       },
       "<"
     );
@@ -162,7 +173,7 @@ document.addEventListener("DOMContentLoaded", function () {
       ".second-family-foreground",
       {
         y: "75%",
-        duration: 1,
+        duration: 5,
       },
       "<"
     );
@@ -170,7 +181,7 @@ document.addEventListener("DOMContentLoaded", function () {
       ".second-family-background",
       {
         y: "75%",
-        duration: 1,
+        duration: 5,
       },
       "<"
     );
@@ -181,7 +192,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //deuxième texte narration
     timelineFirstPart.to(".second-narration-text", {
       opacity: "1",
-      duration: 1,
+      duration: 2,
     });
     timelineFirstPart.to(".second-narration-text", {
       opacity: "1",
@@ -189,12 +200,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     timelineFirstPart.to(".second-narration-text", {
       opacity: "0",
-      duration: 1,
+      duration: 2,
     });
     //troisième texte narration
     timelineFirstPart.to(".third-narration-text", {
       opacity: "1",
-      duration: 1,
+      duration: 2,
     });
     timelineFirstPart.to(".third-narration-text", {
       opacity: "1",
@@ -202,12 +213,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     timelineFirstPart.to(".third-narration-text", {
       opacity: "0",
-      duration: 1,
+      duration: 2,
     });
     //scène tongue
     timelineFirstPart.to(".tong-scene", {
       opacity: "1",
-      duration: 1,
+      duration: 2,
     });
     timelineFirstPart.to(".tong-scene", {
       opacity: "1",
@@ -215,24 +226,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     timelineFirstPart.to(".tong-scene", {
       opacity: "0",
-      duration: 1,
+      duration: 2,
     });
     //partie bulles textes
     timelineFirstPart.to(".part-1", {
       opacity: "1",
-      duration: 1,
-    });
-    timelineFirstPart.to(".part-1", {
-      opacity: "1",
       duration: 2,
     });
     timelineFirstPart.to(".part-1", {
+      opacity: "1",
+      duration: 2,
+    });
+    timelineFirstPart.to(".part-1", {
       opacity: "0",
-      duration: 1,
+      duration: 2,
     });
     timelineFirstPart.to(".part-2", {
       opacity: "1",
-      duration: 1,
+      duration: 2,
     });
     timelineFirstPart.to(".part-2", {
       opacity: "1",
@@ -240,11 +251,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     timelineFirstPart.to(".part-2", {
       opacity: "0",
-      duration: 1,
+      duration: 2,
     });
     timelineFirstPart.to(".part-3", {
       opacity: "1",
-      duration: 1,
+      duration: 2,
     });
     timelineFirstPart.to(".part-3", {
       opacity: "1",
@@ -252,24 +263,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     timelineFirstPart.to(".part-3", {
       opacity: "0",
-      duration: 1,
+      duration: 2,
     });
-    //PARALLAXE EMOTION PART
 
-    // const images = gsap.utils.toArray(".emotion-part img");
-
-    // images.forEach((img, i) => {
-    //   gsap.to(img, {
-    //     yPercent: (i - images.length / 2) * 35, // intensité du décalage (5 = léger)
-    //     ease: "none",
-    //     scrollTrigger: {
-    //       trigger: ".emotion-part",
-    //       start: "top bottom",
-    //       end: "bottom top",
-    //       scrub: 1.5, // inertie fluide (plus grand = plus doux)
-    //     },
-    //   });
-    // });
     //TIMELINE SECOND PART !!!!!!!!!!!////
 
     var timelineSecondPart = gsap.timeline({
@@ -292,13 +288,13 @@ document.addEventListener("DOMContentLoaded", function () {
       ".second-part",
       {
         backgroundColor: "white",
-        duration: 1,
+        duration: 2,
       },
       "<"
     );
     timelineSecondPart.to(".grave-image", {
       opacity: "1",
-      duration: 1,
+      duration: 2,
     });
     timelineSecondPart.to(".grave-image", {
       opacity: "1",
@@ -306,7 +302,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     timelineSecondPart.to(".grave-image", {
       opacity: "0",
-      duration: 1,
+      duration: 2,
     });
 
     //TIMELINE THIRD PART !!!!!!!!!!!////
@@ -322,10 +318,6 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     });
     timelineThirdPart.to(".second-image-third-part", {
-      x: "5%",
-      duration: 0.2,
-    });
-    timelineThirdPart.to(".second-image-third-part", {
       x: "-5%",
       duration: 0.5,
     });
@@ -337,12 +329,15 @@ document.addEventListener("DOMContentLoaded", function () {
         markers: true,
         start: "bottom bottom",
         pin: true,
+        scrub: true,
       },
     });
     timelineBuyPart.to(".buy-button", {
       y: "0%",
       duration: 0.5,
       ease: "power2.out",
+      pin: true,
+      scrub: true,
     });
   }, 1000);
 });
